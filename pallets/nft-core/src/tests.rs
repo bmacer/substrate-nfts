d94e8da7179a981620c44c79915686ab637d4105
bmacer <bmacer@cisco.com>
2021-12-07 00:36:10 -0500

    enable nested ownership

--- pallets/nft-core/src/tests.rs
+++ pallets/nft-core/src/tests.rs
@@ -1,4 +1,5 @@
 use frame_support::{assert_noop, assert_ok, error::BadOrigin};
+use sp_runtime::AccountId32;
 
 // use crate::types::ClassType;
 
@@ -34,10 +35,43 @@ fn mint_nft_works() {
 		assert_ok!(NFTPallet::mint_collection(Origin::signed(ALICE), collection_metadata));
 		assert_ok!(NFTPallet::mint_nft(
 			Origin::signed(ALICE),
+			ALICE,
 			0,
 			Some(ALICE),
 			None,
 			Some(nft_metadata)
 		));
+	});
+}
+
+#[test]
+fn send_nft_to_minted_nft_works() {
+	ExtBuilder::default().build().execute_with(|| {
+		let collection_metadata = stv("testing");
+		let nft_metadata = stv("testing");
+		assert_ok!(NFTPallet::mint_collection(Origin::signed(ALICE), collection_metadata));
+		assert_ok!(NFTPallet::mint_nft(
+			Origin::signed(ALICE),
+			ALICE,
+			0,
+			Some(ALICE),
+			None,
+			Some(nft_metadata.clone())
+		));
+		assert_ok!(NFTPallet::mint_nft(
+			Origin::signed(ALICE),
+			ALICE,
+			0,
+			Some(ALICE),
+			None,
+			Some(nft_metadata)
+		));
+		let a = [1; 32];
+		let x = NFTPallet::send(
+			Origin::signed(ALICE),
+			0,
+			0,
+			AccountIdOrCollectionNftTuple::CollectionAndNftTuple(0, 1),
+		);
 	});
 }
